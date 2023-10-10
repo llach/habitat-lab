27459e6ea40cf791ac2994560ea5bbd4f5c6bb08
llach <lachluca@gmail.com>
2023-10-10 14:30:40 -0400

    make config compatible with new code changes; store episode jsons

--- examples/audio2action.py
+++ examples/audio2action.py
@@ -3,6 +3,8 @@
 # Copyright (c) Meta Platforms, Inc. and its affiliates.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+import gzip 
+
 from omegaconf import OmegaConf
 
 from habitat.datasets.rearrange.rearrange_dataset import RearrangeDatasetV0
@@ -15,13 +17,13 @@ from habitat.datasets.rearrange.run_episode_generator import (
 )
 
 if __name__ == "__main__":
-    vis = True
+    vis = False
     verbose = True
     n_episodes = 1
 
     cfg = get_config_defaults()
-    override_config = OmegaConf.load("examples/rearrange.yaml")
-    # override_config = OmegaConf.load("examples/audio2action.yaml")
+    # override_config = OmegaConf.load("examples/rearrange.yaml")
+    override_config = OmegaConf.load("examples/audio2action.yaml")
     cfg = OmegaConf.merge(cfg, override_config)
 
     dataset = RearrangeDatasetV0()
@@ -32,6 +34,9 @@ if __name__ == "__main__":
     ) as ep_gen:
         print_metadata_mediator(ep_gen)
 
-        # for _ in range(n_episodes):
-        dataset.episodes += ep_gen.generate_episodes(1, verbose)
+        for _ in range(n_episodes):
+            dataset.episodes += ep_gen.generate_episodes(1, verbose)
+
+        with gzip.open("audio2action_ep_dataset.json.gz", "wt") as f:
+            f.write(dataset.to_json())
     pass
